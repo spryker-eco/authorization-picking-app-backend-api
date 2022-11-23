bfe357b6e38826b8ae70bb787ab921cffb987920
Olha Livitchuk <olha.livitchuk@spryker.com>
2022-11-24 00:20:50 +0100

    FRW-458 adjustments

---
+++ tests/config/Shared/config_local.php
@@ -0,0 +1,17 @@
+<?php
+
+use Spryker\Shared\Kernel\KernelConstants;
+
+// ----------------------------------------------------------------------------
+// ------------------------------ CODEBASE: TO REMOVE -------------------------
+// ----------------------------------------------------------------------------
+$config[KernelConstants::PROJECT_NAMESPACE] = 'Pyz';
+$config[KernelConstants::PROJECT_NAMESPACES] = [
+    'Pyz',
+];
+$config[KernelConstants::CORE_NAMESPACES] = [
+    'SprykerShop',
+    'SprykerEco',
+    'Spryker',
+    'SprykerSdk',
+];
