fe3c72eccf280fbf3da5707679cc2bf6790cc472
Olha Livitchuk <olha.livitchuk@spryker.com>
2022-12-19 22:17:20 +0100

    FRW-1101 fixed CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -62,8 +62,9 @@ if (!is_dir($configTargetDirectory)) {
 
 copy($configSourceDirectory . 'config_default.php', $configTargetDirectory . 'config_default.php');
 copy($configSourceDirectory . 'config_propel.php', $configTargetDirectory . 'config_propel.php');
-copy($configSourceDirectory . 'default_store.php', $configTargetDirectory . 'default_store.php');
 copy($configSourceDirectory . 'stores.php', $configTargetDirectory . 'stores.php');
+
+echo 'APPLICATION_STORE = ' . APPLICATION_STORE . PHP_EOL;
 
 $config = Config::getInstance();
 $config->init();
